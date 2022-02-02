3916f7adf14f4cec6187be4993f55eeee5cd323e
Oskar Köök <oskar@maatriks.ee>
2022-02-02 20:19:14 +0200

    measure user ping

--- assets/src/stores/lobby.ts
+++ assets/src/stores/lobby.ts
@@ -1,14 +1,41 @@
-import { Socket } from "phoenix";
+import { Channel, Socket } from "phoenix";
 import { writable } from "svelte/store";
 import { globalAlerts } from "$src/global";
 
 export interface LobbyInfo {
   nodes: string[];
   active_node: string;
+  ping: number;
+};
+
+function measurePing(channel: Channel) {
+  return new Promise<number>((resolve, reject) => {
+    const measurements = [];
+  
+    measure(3);
+    function measure(count: number) {
+      const start = (new Date()).getTime();
+      channel.push("ping", {})
+        .receive("ok", () => {
+          const end = (new Date()).getTime();
+          measurements.push(end - start);
+  
+          if(count > 1) {
+            measure(count - 1);
+          } else {
+            resolve(Math.ceil(measurements.reduce((a, b) => a + b, 0) / measurements.length));
+          }
+        })
+        .receive("error", () => reject("error"))
+        .receive("timeout", () => reject("timeout"));
+    }
+  });
 }
-const { subscribe, set } = writable<LobbyInfo>({
+
+const { subscribe, update } = writable<LobbyInfo>({
   nodes: ["🤔"],
   active_node: "🤔",
+  ping: 0,
 });
 
 export const lobby = {
@@ -17,7 +44,8 @@ export const lobby = {
     const channel = socket.channel("player:lobby", {});
     channel.join()
       .receive("ok", resp => {
-        set(resp);
+        update(info => ({...info, ...resp}));
+        measurePing(channel).then(ping => update(state => ({...state, ping})));
       })
       .receive("error", resp => {
         globalAlerts.push({message: "Could not connect to server!", time: 5000});
