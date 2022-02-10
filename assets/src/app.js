33f1b548652db1869259cdb020e0583ee92b9c46
Oskar Köök <oskar@maatriks.ee>
2022-02-10 17:43:39 +0200

    show alert on socket errors

--- assets/src/app.js
+++ assets/src/app.js
@@ -1,8 +1,12 @@
 import App from "./App.svelte";
 import { Socket } from "phoenix";
 import { lobby } from "./stores/lobby";
+import { globalAlerts } from "$src/global";
 
 const socket = new Socket("/game", {params: {}});
+socket.onError(() => {
+	globalAlerts.push({ message: "Connection error!", time: 4500 });
+});
 socket.connect();
 lobby.connect(socket);
 
