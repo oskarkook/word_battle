25640357efd404e7e6ecf4e3b67d6aec1ad58398
Oskar Köök <oskar@maatriks.ee>
2022-02-24 23:14:22 +0200

    uppercase node names

--- assets/src/lib/NodeList/NodeList.svelte
+++ assets/src/lib/NodeList/NodeList.svelte
@@ -7,15 +7,19 @@
     lobby.leaveQueue();
     lobby.setNode(node);
   }
+
+  function prettify(name: string): string {
+    return name.toUpperCase();
+  }
 </script>
 
 <Button container class="w-10 overflow-x-hidden">
   <select class="px-2 py-1 appearance-none text-center bg-transparent font-bold cursor-pointer" value={$lobby.default_node.node} on:change={handleChange}>
     {#if !$lobby.nodes.some(({ node }) => node === $lobby.default_node.node)}
-      <option value={$lobby.default_node}>{$lobby.default_node.region}</option>
+      <option value={$lobby.default_node}>{prettify($lobby.default_node.region)}</option>
     {/if}
     {#each $lobby.nodes as {node, region}}
-      <option value={node}>{region}</option>
+      <option value={node}>{prettify(region)}</option>
     {/each}
   </select>
 </Button>
