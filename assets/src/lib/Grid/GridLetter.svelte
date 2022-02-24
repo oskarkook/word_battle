f5add3d6740fa0131f040d37fea40da8009a0575
Oskar Köök <oskar@maatriks.ee>
2022-02-24 23:22:37 +0200

    move animations out of tailwind into their relevant components

--- assets/src/lib/Grid/GridLetter.svelte
+++ assets/src/lib/Grid/GridLetter.svelte
@@ -4,6 +4,24 @@
   export let active: boolean = false;
 </script>
 
-<div class:animate-pop={active} class:border-gray-500={active} class={`w-6 h-6 sm:w-14 sm:h-14 inline-flex leading-none text-sm sm:text-3xl text-white m-0.5 items-center justify-center font-bold box-border uppercase transition-colors ${colorClasses[type]} ${type === undefined && "bg-white text-black border-2 border-gray-200"}`}>
+<div class:pop={active} class:border-gray-500={active} class={`w-6 h-6 sm:w-14 sm:h-14 inline-flex leading-none text-sm sm:text-3xl text-white m-0.5 items-center justify-center font-bold box-border uppercase transition-colors ${colorClasses[type]} ${type === undefined && "bg-white text-black border-2 border-gray-200"}`}>
   <slot></slot>
 </div>
+
+<style>
+  @keyframes pop {
+    from {
+      transform: scale(0.8);
+      opacity: 0;
+    }
+
+    60% {
+      transform: scale(1.1);
+      opacity: 1;
+    }
+  }
+
+  .pop {
+    animation: pop 100ms;
+  }
+</style>
--- assets/src/lib/Grid/GridRow.svelte
+++ assets/src/lib/Grid/GridRow.svelte
@@ -12,6 +12,30 @@
   });
 </script>
 
-<div class:animate-shake={shakeTimer !== undefined} class="flex grow w-full h-full">
+<div class:shake={shakeTimer !== undefined} class="flex grow w-full h-full">
   <slot/>
 </div>
+
+<style>
+  @keyframes shake {
+    10%, 90% {
+      transform: translateX(-1px);
+    }
+
+    20%, 80% {
+      transform: translateX(2px);
+    }
+
+    30%, 50%, 70% {
+      transform: translateX(-4px);
+    }
+
+    40%, 60% {
+      transform: translateX(4px);
+    }
+  }
+
+  .shake {
+    animation: shake 600ms;
+  }
+</style>
