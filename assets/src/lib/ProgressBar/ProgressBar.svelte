7a7cba40030387a0cef610f234629856b4b70f55
Oskar Köök <oskar@maatriks.ee>
2022-02-25 13:30:04 +0200

    safari fixes

--- assets/src/lib/ProgressBar/ProgressBar.svelte
+++ assets/src/lib/ProgressBar/ProgressBar.svelte
@@ -5,7 +5,7 @@
   export { className as class };
 </script>
 
-<div class="h-4 w-full bg-gray-200 rounded-full m-1 overflow-hidden">
+<div class="h-4 w-full bg-gray-200 rounded-full m-1 overflow-hidden progress-bar">
   <div
     class="{className} w-full h-full rounded-full move-timer"
     style="margin-left: -{100 - percentage}%; animation-duration: {duration}ms"
@@ -23,6 +23,12 @@
   }
 
   .move-timer {
-    animation: move-timer 1s linear;
+    /* https://stackoverflow.com/a/70837483/ */
+    animation: move-timer 1s linear 0.0001s;
+  }
+
+  .progress-bar {
+    /* https://discourse.webflow.com/t/safari-not-hiding-overflow-on-rounded-corner-divs/55060/2 */
+    -webkit-mask-image: -webkit-radial-gradient(white, black);
   }
 </style>
